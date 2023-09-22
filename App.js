b38c59b894d5fe98dc20a0d4ce6f280adb39ab72
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-22 12:11:04 +0530

    Created a functional component

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Creating a nested complex html structure
-const element = React.createElement("div", { id: "element" }, [
-  React.createElement("div", { id: "firstChild" }, "first child"),
-  React.createElement("div", { id: "secondChild" }, "second child"),
-]);
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Hello This is Tarak</h1>
+    </div>
+  );
+};
 
-const heading = React.createElement("h1", {}, "petPooja using react");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(element);
+root.render(<App />);
