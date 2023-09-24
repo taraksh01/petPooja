9d5f04110da2c8f8a538b404d663f641e98affe4
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-24 19:58:50 +0530

    Passed dynamic data to render the restaurants

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,16 +1,22 @@
-const Restaurant = () => {
+import { RESTAURANT_IMAGE_URL } from "../constants";
+
+const Restaurant = ({ info }) => {
+  // const { name, avgRatingString, constForTwo, cuisines } = info;
   return (
-    <div className="w-1/6 bg-gray-50 rounded-2xl cursor-pointer hover:bg-gray-100">
-      <img
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ee5f8e06b300efc07c9fe3f4df40dfc4"
-        alt="restaurant image"
-        className="w-full rounded-2xl p-0"
-      />
+    <div className="w-full md:w-1/5 bg-gray-50 rounded-2xl cursor-pointer flex flex-col hover:bg-gray-100">
+      <div className="object-cover w-full h-52 drop-shadow">
+        <img
+          src={RESTAURANT_IMAGE_URL + info?.cloudinaryImageId}
+          alt="restaurant image"
+          className="w-full rounded-2xl p-0 h-full"
+        />
+      </div>
+      {console.log(info)}
       <div className="pl-2">
-        <h2 className="text-lg py-1 font-normal">Restaurant name</h2>
-        <p className="font-light text-gray-600">ratings</p>
-        <p className="font-light text-gray-600">Restaurant cuisies</p>
-        <p className="font-light text-gray-600">price</p>
+        <h2 className="text-lg py-1 font-normal">{info?.name}</h2>
+        <p className="font-light text-gray-600">{info?.avgRatingString}</p>
+        <p className="font-light text-gray-600">{info?.cuisines.join(", ")}</p>
+        <p className="font-light text-gray-600">{info?.costForTwo}</p>
       </div>
     </div>
   );
--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,9 +1,12 @@
 import Restaurant from "./Restaurant";
+import { restaurant } from "../constants";
 
 const RestaurantList = () => {
   return (
-    <div className="flex flex-wrap justify-center gap-4 m-2 w-10/12 mx-auto">
-      <Restaurant />
+    <div className="flex flex-col md:flex-row flex-wrap justify-center gap-4 m-2 w-10/12 mx-auto">
+      {restaurant.map((res) => (
+        <Restaurant info={res?.info} />
+      ))}
     </div>
   );
 };
