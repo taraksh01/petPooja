9ede63e2d111cbfba7c7947f2d372194d4e8d983
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-24 20:36:43 +0530

    Passed key to restaurant component

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -5,7 +5,7 @@ const RestaurantList = () => {
   return (
     <div className="flex flex-col md:flex-row flex-wrap justify-center gap-4 m-2 w-10/12 mx-auto">
       {restaurant.map((res) => (
-        <Restaurant info={res?.info} />
+        <Restaurant info={res?.info} key={res?.info?.id} />
       ))}
     </div>
   );
