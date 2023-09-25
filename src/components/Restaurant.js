95a0dff6ff117c347263b45ec66a3c1f2ac282a2
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-25 18:30:02 +0530

    Extracted individual property from info and displayed

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,21 +1,22 @@
 import { RESTAURANT_IMAGE_URL } from "../constants";
 
 const Restaurant = ({ info }) => {
-  // const { name, avgRatingString, constForTwo, cuisines } = info;
+  const { name, avgRatingString, costForTwo, cuisines, cloudinaryImageId } =
+    info;
   return (
     <div className="w-full md:w-1/5 bg-gray-50 rounded-2xl cursor-pointer flex flex-col hover:bg-gray-100">
       <div className="object-cover w-full h-52 drop-shadow">
         <img
-          src={RESTAURANT_IMAGE_URL + info?.cloudinaryImageId}
+          src={RESTAURANT_IMAGE_URL + cloudinaryImageId}
           alt="restaurant image"
           className="w-full rounded-2xl p-0 h-full"
         />
       </div>
       <div className="pl-2">
-        <h2 className="text-lg py-1 font-normal">{info?.name}</h2>
-        <p className="font-light text-gray-600">{info?.avgRatingString}</p>
-        <p className="font-light text-gray-600">{info?.cuisines.join(", ")}</p>
-        <p className="font-light text-gray-600">{info?.costForTwo}</p>
+        <h2 className="text-lg py-1 font-normal">{name}</h2>
+        <p className="font-light text-gray-600">{avgRatingString}</p>
+        <p className="font-light text-gray-600">{cuisines.join(", ")}</p>
+        <p className="font-light text-gray-600">{costForTwo}</p>
       </div>
     </div>
   );
