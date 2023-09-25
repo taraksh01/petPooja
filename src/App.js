7771467708949cc8514f5678b105a9628bad3a99
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-25 14:16:30 +0530

    Added serachText state variable and passed it to Header component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import RestaurantList from "./components/RestaurantList";
+import { useState } from "react";
+
 const App = () => {
+  const [searchText, setSearchText] = useState("");
+
+  function handleSearch(newSearchText) {
+    setSearchText(newSearchText);
+  }
   return (
     <>
-      <Header />
+      <Header
+        searchText={searchText}
+        onClick={() => handle()}
+        handleSearch={handleSearch}
+      />
       <RestaurantList />
     </>
   );
