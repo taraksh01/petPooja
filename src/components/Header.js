9a8842e77e60f43b61234844f6adb1bc3f35c9a4
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-25 14:20:03 +0530

    Received props from App component and send it to Search component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
-const Header = () => {
+import Search from "./Search";
+
+const Header = ({ searchText, handleSearch }) => {
   return (
     <header className="flex justify-between items-center bg-gray-300 p-2">
       <h1 className="text-5xl">logo</h1>
+      <Search searchText={searchText} handleSearch={handleSearch} />
       <nav className="flex gap-4 text-xl">
         <p>Offer</p>
         <p>Cart</p>
