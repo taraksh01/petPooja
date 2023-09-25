5e1e43ba0dca8811b48a7c5d46771b5fcfb5a718
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-25 18:35:41 +0530

    Created two state variables allRestaurants and filteredRestaurants and passed filteredRestaurants to RestaurantList component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import RestaurantList from "./components/RestaurantList";
 import { useState } from "react";
+import { restaurant } from "./constants";
 
 const App = () => {
   const [searchText, setSearchText] = useState("");
+  const [allRestaurants, setAllRestaurants] = useState(restaurant);
+  const [filteredRestaurants, setFilteredRestaurants] =
+    useState(allRestaurants);
 
   function handleSearch(newSearchText) {
     setSearchText(newSearchText);
   }
   return (
     <>
-      <Header
-        searchText={searchText}
-        onClick={() => handle()}
-        handleSearch={handleSearch}
-      />
-      <RestaurantList />
+      <Header searchText={searchText} handleSearch={handleSearch} />
+      <RestaurantList restaurants={filteredRestaurants} />
     </>
   );
 };
