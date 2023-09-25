c368edddb61888fec050141f437dea6259b09e5a
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-25 20:39:03 +0530

    Created component to show message to search something else

---
+++ src/components/NoSearchResult.js
@@ -0,0 +1,9 @@
+const NoSearchResult = () => {
+  return (
+    <h2 className="flex justify-center items-center text-4xl">
+      Try searching for something else
+    </h2>
+  );
+};
+
+export default NoSearchResult;
