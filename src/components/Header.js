8cd50ce9dcda157af4aa8738b47fdf5a644d1374
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-25 18:55:51 +0530

    Forwarded filteredRestaurants, setFilteredRestaurnats and allRestaurants to Search component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
 import Search from "./Search";
 
-const Header = ({ searchText, handleSearch }) => {
+const Header = ({
+  searchText,
+  handleSearch,
+  filteredRestaurants,
+  setFilteredRestaurants,
+  allRestaurants,
+}) => {
   return (
     <header className="flex justify-between items-center bg-gray-300 p-2">
       <h1 className="text-5xl">logo</h1>
-      <Search searchText={searchText} handleSearch={handleSearch} />
+      <Search
+        searchText={searchText}
+        handleSearch={handleSearch}
+        filteredRestaurants={filteredRestaurants}
+        setFilteredRestaurants={setFilteredRestaurants}
+        allRestaurants={allRestaurants}
+      />
       <nav className="flex gap-4 text-xl">
         <p>Offer</p>
         <p>Cart</p>
