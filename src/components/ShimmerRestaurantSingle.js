044c61da937d0ced2b9043f226fb33623a7d95ca
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-25 20:10:18 +0530

    Created Shimmer UI for individual restaurant component

---
+++ src/components/ShimmerRestaurantSingle.js
@@ -0,0 +1,13 @@
+const ShimmerRestaurantSingle = () => {
+  return (
+    <div className="w-full md:w-1/5 rounded-2xl flex flex-col">
+      <div className="w-full h-52 bg-gray-200 rounded-2xl"></div>
+      <p className="h-4 rounded-md bg-gray-200 mx-1 my-2"></p>
+      <p className="h-3 rounded-md bg-gray-200 my-1 mx-1"></p>
+      <p className="h-3 rounded-md bg-gray-200 my-1 mx-1"></p>
+      <p className="h-3 rounded-md bg-gray-200 my-1 mx-1"></p>
+    </div>
+  );
+};
+
+export default ShimmerRestaurantSingle;
