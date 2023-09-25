b93a6ffb7b4fff39eb178a9eed4bfcf896d2c205
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-25 20:10:59 +0530

    Created Shimmer UI for restaurantList component

---
+++ src/components/ShimmerRestaurantList.js
@@ -0,0 +1,26 @@
+import ShimmerRestaurantSingle from "./ShimmerRestaurantSingle";
+
+const ShimmerRestaurantList = () => {
+  return (
+    <div className="flex flex-col md:flex-row flex-wrap justify-center gap-4 m-2 w-10/12 mx-auto">
+      <ShimmerRestaurantSingle />
+      <ShimmerRestaurantSingle />
+      <ShimmerRestaurantSingle />
+      <ShimmerRestaurantSingle />
+      <ShimmerRestaurantSingle />
+      <ShimmerRestaurantSingle />
+      <ShimmerRestaurantSingle />
+      <ShimmerRestaurantSingle />
+      <ShimmerRestaurantSingle />
+      <ShimmerRestaurantSingle />
+      <ShimmerRestaurantSingle />
+      <ShimmerRestaurantSingle />
+      <ShimmerRestaurantSingle />
+      <ShimmerRestaurantSingle />
+      <ShimmerRestaurantSingle />
+      <ShimmerRestaurantSingle />
+    </div>
+  );
+};
+
+export default ShimmerRestaurantList;
