fbf0453a6246c13ea905a39e25d76e6786d407be
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-26 09:56:35 +0530

    Instead of copy pasting used an array to itterate and call the ShimmerRestaurantSingle component

--- src/components/ShimmerRestaurantList.js
+++ src/components/ShimmerRestaurantList.js
@@ -3,22 +3,11 @@ import ShimmerRestaurantSingle from "./ShimmerRestaurantSingle";
 const ShimmerRestaurantList = () => {
   return (
     <div className="flex flex-col md:flex-row flex-wrap justify-center gap-4 m-2 w-10/12 mx-auto">
-      <ShimmerRestaurantSingle />
-      <ShimmerRestaurantSingle />
-      <ShimmerRestaurantSingle />
-      <ShimmerRestaurantSingle />
-      <ShimmerRestaurantSingle />
-      <ShimmerRestaurantSingle />
-      <ShimmerRestaurantSingle />
-      <ShimmerRestaurantSingle />
-      <ShimmerRestaurantSingle />
-      <ShimmerRestaurantSingle />
-      <ShimmerRestaurantSingle />
-      <ShimmerRestaurantSingle />
-      <ShimmerRestaurantSingle />
-      <ShimmerRestaurantSingle />
-      <ShimmerRestaurantSingle />
-      <ShimmerRestaurantSingle />
+      {Array(16)
+        .fill()
+        .map((a, index) => (
+          <ShimmerRestaurantSingle key={index} />
+        ))}
     </div>
   );
 };
