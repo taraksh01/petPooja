48121db1e7d931891d91548398c75e0958999aeb
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-26 19:38:33 +0530

    Fetched data for details page

--- src/components/RestaurantMenuDetails.js
+++ src/components/RestaurantMenuDetails.js
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { SWIGGY_RESTAURANT_Details_URL } from "../constants";
 
 const RestaurantMenuDetails = () => {
   const { id } = useParams();
+
+  useEffect(() => {
+    fetchRestaurantDetails();
+  }, []);
+
+  const fetchRestaurantDetails = async () => {
+    const data = await fetch(SWIGGY_RESTAURANT_Details_URL + id);
+    const json = await data?.json();
+
+    console.log(json);
+  };
   return <main className="">{id}</main>;
 };
 
