ea039c789ec9aa37b203f932337a06191b34d533
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-26 16:14:31 +0530

    Moved Search component out of Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,7 @@
-import Search from "./Search";
-
-const Header = ({
-  searchText,
-  handleSearch,
-  filteredRestaurants,
-  setFilteredRestaurants,
-  allRestaurants,
-}) => {
+const Header = ({}) => {
   return (
     <header className="flex justify-between items-center bg-gray-300 p-2">
       <h1 className="text-5xl">logo</h1>
-      <Search
-        searchText={searchText}
-        handleSearch={handleSearch}
-        filteredRestaurants={filteredRestaurants}
-        setFilteredRestaurants={setFilteredRestaurants}
-        allRestaurants={allRestaurants}
-      />
       <nav className="flex gap-4 text-xl">
         <p>Offer</p>
         <p>Cart</p>
