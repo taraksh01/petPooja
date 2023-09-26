f015a832a76528917c87e004e411e564ecc2880a
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-26 10:04:47 +0530

    Rendering NoSearchResult if the restaurants array is empty

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,8 +1,11 @@
 import Restaurant from "./Restaurant";
+import NoSearchResult from "./NoSearchResult";
+
 
 const RestaurantList = ({ restaurants }) => {
   return (
     <div className="flex flex-col md:flex-row flex-wrap justify-center gap-4 m-2 w-10/12 mx-auto">
+      {restaurants?.length == 0 && <NoSearchResult />}
       {restaurants?.map((res) => (
         <Restaurant info={res?.info} key={res?.info?.id} />
       ))}
