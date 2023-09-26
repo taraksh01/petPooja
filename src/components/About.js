f5b06a3743d0294765c3f120bc5c7f9201ecf6f5
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-26 16:42:30 +0530

    Added min height to 80vh for About and Contact components

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 const About = () => {
   return (
-    <div className="text-center text-gray-700 m-5">
+    <div className="text-center text-gray-700 m-5 min-h-[80vh]">
       <h1 className="text-3xl">This is the about page for petpooja.</h1>
       <p className="text-2xl leading-10">
         petpooja is an early stage food delivery app that do nothing 😄😄.
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 const Contact = () => {
   return (
-    <div className="text-gray-700 text-center m-5">
+    <div className="text-gray-700 text-center m-5 min-h-[80vh]">
       <h1 className="text-3xl">This is the contact page of petpooja.</h1>
       <p className="text-2xl leading-10">
         If you need any help please don't ask us. Resolve it on your own 😴.
