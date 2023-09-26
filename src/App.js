354fe4ebb33696a9bc360b87ae2e00d665519e10
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-26 16:25:02 +0530

    Moved Search feature and RestaurantList component to RestaurantOverview component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,17 @@
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
-import RestaurantList from "./components/RestaurantList";
-import ShimmerRestaurantList from "./components/ShimmerRestaurantList";
+import RestaurantOverview from "./components/RestaurantOverview";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { SWIGGY_RESTAURANT_API_URL } from "./constants";
 
 const App = () => {
-  const [searchText, setSearchText] = useState("");
-  const [allRestaurants, setAllRestaurants] = useState([]);
-  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
-
-  useEffect(() => {
-    fetchRestaurants();
-  }, []);
-
-  const fetchRestaurants = async () => {
-    const data = await fetch(SWIGGY_RESTAURANT_API_URL);
-    const json = await data?.json();
-
-    setAllRestaurants(
-      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
-
-    setFilteredRestaurants(
-      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
-  };
-
-  function handleSearch(newSearchText) {
-    setSearchText(newSearchText);
-  }
-
   return (
     <>
-      <Header
-        searchText={searchText}
-        handleSearch={handleSearch}
-        filteredRestaurants={filteredRestaurants}
-        setFilteredRestaurants={setFilteredRestaurants}
-        allRestaurants={allRestaurants}
-      />
-      {allRestaurants?.length == 0 ? (
-        <ShimmerRestaurantList />
-      ) : (
-        <RestaurantList restaurants={filteredRestaurants} />
-      )}
+      <Header />
+      <RestaurantOverview />
       <Footer />
     </>
   );
