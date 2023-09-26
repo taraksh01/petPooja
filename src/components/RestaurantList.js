7c5237ea74e82679eabcd54b2d43910560f15838
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-26 21:23:01 +0530

    Wrapped Restaurant component inside Link

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,12 +1,19 @@
 import Restaurant from "./Restaurant";
 import NoSearchResult from "./NoSearchResult";
+import { Link } from "react-router-dom";
 
 const RestaurantList = ({ restaurants }) => {
   return (
     <div className="flex flex-col md:flex-row flex-wrap justify-center gap-4 m-2 w-10/12 mx-auto">
       {restaurants?.length == 0 && <NoSearchResult />}
       {restaurants?.map((res) => (
-        <Restaurant info={res?.info} key={res?.info?.id} />
+        <Link
+          to={"/restaurant/" + res?.info?.id}
+          key={res?.info?.id}
+          className="w-full md:w-1/5"
+        >
+          <Restaurant info={res?.info} />
+        </Link>
       ))}
     </div>
   );
