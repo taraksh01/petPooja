15faa5d1b3ce5f8cf33cd520fe80b5bb68d228bf
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-26 19:36:41 +0530

    Stored swiggy's restaurant details page api url

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,6 @@ export const RESTAURANT_IMAGE_URL =
 
 export const SWIGGY_RESTAURANT_API_URL =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+export const SWIGGY_RESTAURANT_Details_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
