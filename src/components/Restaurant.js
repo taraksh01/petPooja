9c6c2939584870add37f3047c722fd72c14b5d9f
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-26 21:23:38 +0530

    Changed width for large device

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -4,8 +4,8 @@ const Restaurant = ({ info }) => {
   const { name, avgRatingString, costForTwo, cuisines, cloudinaryImageId } =
     info;
   return (
-    <div className="w-full md:w-1/5 bg-gray-50 rounded-2xl cursor-pointer flex flex-col hover:bg-gray-100">
-      <div className="object-cover w-full h-52 drop-shadow">
+    <div className="w-full bg-gray-50 rounded-2xl cursor-pointer flex flex-col hover:bg-gray-200">
+      <div className="object-cover w-full h-52">
         <img
           src={RESTAURANT_IMAGE_URL + cloudinaryImageId}
           alt="restaurant image"
