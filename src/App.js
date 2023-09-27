39728e6b683f4062e743eeffecd98ab8601af251
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-27 20:02:25 +0530

    Lazy loading About and Contact component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Header from "./components/Header";
 import RestaurantOverview from "./components/RestaurantOverview";
 import RestaurantMenuDetails from "./components/RestaurantMenuDetails";
 import Footer from "./components/Footer";
-import About from "./components/About";
-import Contact from "./components/Contact";
 import Error from "./components/Error";
 import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
+import { Suspense, lazy } from "react";
 
+const About = lazy(() => import("./components/About"));
+const Contact = lazy(() => import("./components/Contact"));
 const App = () => {
   return (
     <>
@@ -25,8 +26,24 @@ const appConfig = createBrowserRouter([
     errorElement: <Error />,
     children: [
       { path: "/", element: <RestaurantOverview />, errorElement: <Error /> },
-      { path: "/about", element: <About />, errorElement: <Error /> },
-      { path: "/contact", element: <Contact />, errorElement: <Error /> },
+      {
+        path: "/about",
+        element: (
+          <Suspense>
+            <About />
+          </Suspense>
+        ),
+        errorElement: <Error />,
+      },
+      {
+        path: "/contact",
+        element: (
+          <Suspense>
+            <Contact />
+          </Suspense>
+        ),
+        errorElement: <Error />,
+      },
       { path: "/restaurant/:id", element: <RestaurantMenuDetails /> },
     ],
   },
