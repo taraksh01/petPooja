87c08dfdd1438ced940efd9ab865d4043dc8bab0
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-11-23 21:37:42 +0530

    Added api url for mobile device

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,10 @@ export const RESTAURANT_IMAGE_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const SWIGGY_RESTAURANT_API_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627";
+
+export const SWIGGY_RESTAURANT_API_MOBILE_URL =
+  "https://www.swiggy.com/mapi/homepage/getCards?lat=22.572646&lng=88.36389500000001";
 
 export const SWIGGY_RESTAURANT_Details_URL =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
