6897172df2d01da0074eaf7ee58cc6e8cb03c1a0
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-16 16:47:02 +0530

    Made restaurantlist to display as grid elements

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 
 const RestaurantList = ({ restaurants }) => {
   return (
-    <div className="flex flex-col md:flex-row flex-wrap justify-center gap-4 m-2 w-10/12 mx-auto">
+    <div className="grid grid-rows-2 grid-cols-1 sm:grid-cols-4 justify-evenly gap-5 m-2 w-3/4 mx-auto">
       {restaurants?.length == 0 && <NoSearchResult />}
       {restaurants?.map((res) => (
         <Link
           to={"/restaurant/" + res?.info?.id}
           key={res?.info?.id}
-          className="w-full md:w-1/5"
+          className="w-full"
         >
           <Restaurant info={res?.info} />
         </Link>
