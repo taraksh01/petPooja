c9633d9898bf4553745ce62b4b71d33e4f52fc01
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-16 17:42:40 +0530

    Showing no search result outside the restaurant list

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -4,18 +4,23 @@ import { Link } from "react-router-dom";
 
 const RestaurantList = ({ restaurants }) => {
   return (
-    <div className="grid grid-rows-2 grid-cols-1 sm:grid-cols-4 justify-evenly gap-5 m-2 w-3/4 mx-auto">
-      {restaurants?.length == 0 && <NoSearchResult />}
-      {restaurants?.map((res) => (
-        <Link
-          to={"/restaurant/" + res?.info?.id}
-          key={res?.info?.id}
-          className="w-full"
-        >
-          <Restaurant info={res?.info} />
-        </Link>
-      ))}
-    </div>
+    <>
+      {restaurants?.length == 0 ? (
+        <NoSearchResult />
+      ) : (
+        <div className="grid grid-rows-2 grid-cols-1 sm:grid-cols-4 justify-evenly gap-5 m-2 w-3/4 mx-auto">
+          {restaurants?.map((res) => (
+            <Link
+              to={"/restaurant/" + res?.info?.id}
+              key={res?.info?.id}
+              className="w-full"
+            >
+              <Restaurant info={res?.info} />
+            </Link>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
