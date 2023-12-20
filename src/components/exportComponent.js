6b8621e74057128fb8443d0f5996467593335ac1
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-20 20:48:40 +0530

    Created a common file to export all components

---
+++ src/components/exportComponent.js
@@ -0,0 +1,21 @@
+import Restaurant from "./Restaurant";
+import RestaurantList from "./RestaurantList";
+import RestaurantMenuDetails from "./RestaurantMenuDetails";
+import RestaurantOverview from "./RestaurantOverview";
+import Search from "./Search";
+import ShimmerRestaurantList from "./ShimmerRestaurantList";
+import ShimmerRestaurantSingle from "./ShimmerRestaurantSingle";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export {
+  Restaurant,
+  RestaurantList,
+  RestaurantMenuDetails,
+  RestaurantOverview,
+  Search,
+  ShimmerRestaurantList,
+  ShimmerRestaurantSingle,
+  Header,
+  Footer,
+};
