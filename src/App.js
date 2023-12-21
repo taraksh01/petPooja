ddd14b56504886889bd5b7aaa6c77d3b840f0422
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-21 10:57:43 +0530

    Dispatching location details if it is present in local storage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { useDispatch } from "react-redux";
 import { Header, Footer } from "./components/exportComponent";
 import { Outlet } from "react-router-dom";
+import { locationDetails } from "./Store/locationSlice";
 
 const App = () => {
+  const userLocation = localStorage.getItem("userLocation");
+  const dispatch = useDispatch();
+  userLocation && dispatch(locationDetails(JSON.parse(userLocation)));
   return (
     <>
       <Header />
