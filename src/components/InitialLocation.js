5558f8e98bed13bc94ae1909af4dfe07b6023976
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-22 20:45:03 +0530

    Removed Location component from body

--- src/components/InitialLocation.js
+++ src/components/InitialLocation.js
@@ -1,9 +1,6 @@
-import { Location } from "./exportComponent";
-
 const InitialLocation = () => {
   return (
     <main className="bg-gray-200 py-80">
-      <Location />
       <h1 className="text-3xl text-center p-5">
         Where would you like to find restaurants?
       </h1>
