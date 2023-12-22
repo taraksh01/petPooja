3ebc7f1def0b654942c27f0b6d4f44cb9f0b278a
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-22 18:21:28 +0530

    Storing restaurants in redux store

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cartSlice";
 import locationSlice from "./locationSlice";
+import restaurants from "./restaurants";
 
 const store = configureStore({
   reducer: {
     cart: cartSlice,
     location: locationSlice,
+    restaurants,
   },
 });
 
