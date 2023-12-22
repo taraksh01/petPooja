bcf9bfc4f15293b6f5edaccd4998fb47a9b2e91f
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-22 19:34:58 +0530

    Showing location info

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,44 @@
 import { useSelector } from "react-redux";
+import { Location } from "./exportComponent";
+import fetchRestaurants from "../utils/fetchRestaurants";
+import { useState } from "react";
 
 const Header = ({}) => {
   const cartItem = useSelector((store) => store.cart.items);
-
+  const [hideInputBox, setHideInputBox] = useState(true);
+  const location = useSelector((store) => store?.location?.details);
+  if (location?.place_id) {
+    fetchRestaurants();
+  }
   return (
-    <header className="flex justify-between items-center bg-gray-300 p-2">
-      <h1 className="text-5xl">logo</h1>
-      <nav className="flex gap-4 text-xl">
-        <p>Offer</p>
-        <p>
+    <header className="flex justify-between items-center bg-gray-900 text-white p-2">
+      <h1 className="text-3xl font-medium text-orange-400">PetPooja</h1>
+      {hideInputBox ? (
+        <div>
+          <h2
+            className="text-xl sm:text-2xl"
+            onClick={() => setHideInputBox(!hideInputBox)}
+          >
+            <span className="font-semibold underline cursor-pointer px-2">
+              Other
+            </span>
+            {location?.display_name}
+          </h2>
+        </div>
+      ) : (
+        <Location
+          hideSearchearch={hideInputBox}
+          setHideSearch={setHideInputBox}
+        />
+      )}
+      <nav className="flex text-xl">
+        <p className="font-medium">
           Cart
-          <span className="font-bold text-sm relative bottom-2 left-1">
+          <span className="font-bold text-orange-400 text-sm relative bottom-2 left-1">
             {cartItem.length}
           </span>
         </p>
       </nav>
-      {/* <div className="text-xl text-red-600">{user.name}</div> */}
     </header>
   );
 };
