ea6519729f7033c96d0c7998f6cba3d4fb06d547
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-22 19:03:55 +0530

    Showing Location unserviceable when swiggy data is not present for current location

--- src/components/RestaurantOverview.js
+++ src/components/RestaurantOverview.js
@@ -10,6 +10,7 @@ import { useSelector } from "react-redux";
 
 const RestaurantOverview = () => {
   const userLocation = useSelector((store) => store?.location?.details);
+  const isAvailable = useSelector((store) => store?.restaurants?.available);
   const allRestaurants = useSelector(
     (store) => store?.restaurants?.restaurants
   );
@@ -35,6 +36,21 @@ const RestaurantOverview = () => {
       <h1 className="flex justify-center items-center h-[80vh] text-5xl">
         Check your internet connection
       </h1>
+    );
+  }
+
+  if (!isAvailable) {
+    return (
+      <div className="flex justify-center items-center max-w-3xl mx-auto relative text-2xl sm:text-3xl h-screen bg-black">
+        <img
+          src="https://res.cloudinary.com/swiggy/image/upload/portal/m/location_unserviceable"
+          alt="Location Unserviceable"
+          className="w-full object-contain"
+        />
+        <h1 className="absolute bottom-1/2 font-bold">
+          Location Unserviceable 😔
+        </h1>
+      </div>
     );
   }
 
