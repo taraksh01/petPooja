a8306120ed4742d12f150d61b07267c9eaf16c5f
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-23 17:06:14 +0530

    Added best offers banner at top of page when available

--- src/components/RestaurantOverview.js
+++ src/components/RestaurantOverview.js
@@ -7,10 +7,12 @@ import {
 } from "./exportComponent";
 import useOnline from "../utils/useOnline";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const RestaurantOverview = () => {
   const userLocation = useSelector((store) => store?.location?.details);
   const isAvailable = useSelector((store) => store?.restaurants?.available);
+  const banner = useSelector((store) => store?.restaurants?.banner);
   const allRestaurants = useSelector(
     (store) => store?.restaurants?.restaurants
   );
@@ -55,7 +57,24 @@ const RestaurantOverview = () => {
   }
 
   return (
-    <div className="flex flex-col items-center gap-5">
+    <div className="flex flex-col w-5/6 lg:w-3/4 mx-auto gap-5">
+      {banner?.length > 0 && (
+        <div className="m-2">
+          <h2 className="text-2xl font-bold">Best offers</h2>
+          <div className="flex gap-5 overflow-auto no-scrollbar my-2">
+            {banner?.map((item) => (
+              <Link to={``} key={item?.entityId} className="min-w-fit">
+                <div>
+                  <img
+                    src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_400/${item?.imageId}`}
+                    alt={item?.imageId}
+                  />
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
       <Search
         searchText={searchText}
         handleSearch={handleSearch}
