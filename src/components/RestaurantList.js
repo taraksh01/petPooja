069b2235b3c07717280e449b734cc6eae9ff7445
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-24 10:59:01 +0530

    Displaying restaurant list as flex if it is top restaurants else as grid

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -2,18 +2,24 @@ import Restaurant from "./Restaurant";
 import NoSearchResult from "./NoSearchResult";
 import { Link } from "react-router-dom";
 
-const RestaurantList = ({ restaurants }) => {
+const RestaurantList = ({ top, restaurants }) => {
   return (
     <>
       {restaurants?.length == 0 ? (
         <NoSearchResult />
       ) : (
-        <div className="grid grid-rows-2 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-evenly gap-5 m-2 w-full">
+        <div
+          className={`${
+            top
+              ? `flex gap-5 overflow-auto no-scrollbar my-2`
+              : `grid grid-rows-2 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-evenly gap-5 m-2 w-full`
+          }`}
+        >
           {restaurants?.map((res) => (
             <Link
               to={"/restaurant/" + res?.info?.id}
               key={res?.info?.id}
-              className="w-full"
+              className="min-w-fit"
             >
               <Restaurant info={res?.info} />
             </Link>
