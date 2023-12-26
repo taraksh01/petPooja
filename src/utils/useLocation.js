faeb0b55cb9a906f2405d0cf97eaba4d8cf13210
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-26 20:41:14 +0530

    Added api key for geocode

--- src/utils/useLocation.js
+++ src/utils/useLocation.js
@@ -1,5 +1,7 @@
 const useLocation = async (city) => {
-  const coOrdinates = await fetch(`https://geocode.maps.co/search?q=${city}`);
+  const coOrdinates = await fetch(
+    `https://geocode.maps.co/search?q=${city}&api_key=${process.env.GEOCODE_API_KEY}`
+  );
   const json = await coOrdinates?.json();
   return json.length > 0 ? json[0] : "Error";
 };
