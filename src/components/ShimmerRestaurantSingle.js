f59af4c07ce14b9d9dc1b9059ee33d8791276c05
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-26 20:42:08 +0530

    Maded Restaurant shimmer align with restaurant style

--- src/components/ShimmerRestaurantSingle.js
+++ src/components/ShimmerRestaurantSingle.js
@@ -1,6 +1,6 @@
 const ShimmerRestaurantSingle = () => {
   return (
-    <div className="w-full md:w-1/5 rounded-2xl flex flex-col">
+    <div className="w-full max-w-sm bg-gray-100 rounded-2xl grid grid-rows-1 grid-cols-1">
       <div className="w-full h-52 bg-gray-200 rounded-2xl"></div>
       <p className="h-4 rounded-md bg-gray-200 mx-1 my-2"></p>
       <p className="h-3 rounded-md bg-gray-200 my-1 mx-1"></p>
