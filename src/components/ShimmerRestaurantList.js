6863afee6bc620538862a5d36ea2e2fa172787b7
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-26 20:42:57 +0530

    Made RestaurantList shimmer align with restaurantlist ui

--- src/components/ShimmerRestaurantList.js
+++ src/components/ShimmerRestaurantList.js
@@ -2,7 +2,8 @@ import ShimmerRestaurantSingle from "./ShimmerRestaurantSingle";
 
 const ShimmerRestaurantList = () => {
   return (
-    <div className="flex flex-col md:flex-row flex-wrap justify-center gap-4 m-2 w-10/12 mx-auto">
+    <div className="grid grid-rows-1 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center gap-5 my-2 w-full">
+      {/* <div className="flex flex-col md:flex-row flex-wrap justify-center gap-4 m-2 w-10/12 mx-auto"> */}
       {Array(16)
         .fill()
         .map((a, index) => (
